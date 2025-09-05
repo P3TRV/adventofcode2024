8970d3c741b88046c650482b3b8902b799e19488
petrv <petrv@HAL3000.fritz.box>
2025-09-05 13:54:47 +0200

    adventofcode2024 - day1

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * Advent of code 2024
+ * 
+ * https://adventofcode.com/2024
+ */
+module day1 {
+}
---
+++ src/xms/adventofcode/day1/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Advent of code 2024
+ * 
+ * https://adventofcode.com/2024
+ */
+package xms.adventofcode.day1;
